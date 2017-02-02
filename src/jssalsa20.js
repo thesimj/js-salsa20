15c6f6a5ddfc0880e4025536cb87980416f80803
Mykola Bubelich <thesimj@users.noreply.github.com>
2017-02-02 16:58:28 +0100

    Apply standard js and rewrite to ES5 compatibility. Rewrite rounds function

--- src/jssalsa20.js
+++ src/jssalsa20.js
@@ -1,3 +1,4 @@
+'use strict'
 /*
  * Copyright (c) 2017, Bubelich Mykola
  * https://www.bubelich.com
@@ -43,205 +44,203 @@
  * public domain implementations optimized for common architectures.
  */
 
-class JSSalsa20 {
-
-  /**
-   * Construct SalSa20 instance with key and nonce
-   * Key should be Uint8Array with 32 bytes
-   * None should be Uint8Array with 8 bytes
-   *
-   *
-   * @throws {Error}
-   * @param {Uint8Array} key
-   * @param {Uint8Array} nonce
-   */
-  constructor(key, nonce) {
-    if (!(key instanceof Uint8Array) || key.length !== 32) {
-      throw new Error("Key should be 32 byte array!");
-    }
-
-    if (!(nonce instanceof Uint8Array) || nonce.length !== 8) {
-      throw new Error("Nonce should be 8 byte array!");
-    }
-
-    this.rounds = 20;
-    this.sigma = [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574];
-
-    this.param = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-    // set sigma constant //
-    this.param[0] = this.sigma[0];
-    this.param[5] = this.sigma[1];
-    this.param[10] = this.sigma[2];
-    this.param[15] = this.sigma[3];
-
-    // set key //
-    this.param[1] = this._get32(key, 0);
-    this.param[2] = this._get32(key, 4);
-    this.param[3] = this._get32(key, 8);
-    this.param[4] = this._get32(key, 12);
-
-    // set nonce //
-    this.param[6] = this._get32(nonce, 0);
-    this.param[7] = this._get32(nonce, 4);
-
-    // set counter //
-    this.param[8] = 0;
-    this.param[9] = 0;
-
-    // set key again //
-    this.param[11] = this._get32(key, 16);
-    this.param[12] = this._get32(key, 20);
-    this.param[13] = this._get32(key, 24);
-    this.param[14] = this._get32(key, 28);
-
-    // log param
-    // this._log(this.param, "param");
-
-    // init block //
-    this.block = [
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    ];
-
-    // internal byte counter //
-    this.byteCounter = 0;
+/**
+ * Construct SalSa20 instance with key and nonce
+ * Key should be Uint8Array with 32 bytes
+ * None should be Uint8Array with 8 bytes
+ *
+ *
+ * @throws {Error}
+ * @param {Uint8Array} key
+ * @param {Uint8Array} nonce
+ */
+var JSSalsa20 = function (key, nonce) {
+  if (!(key instanceof Uint8Array) || key.length !== 32) {
+    throw new Error('Key should be 32 byte array!')
   }
 
-  /**
-   *  Encrypt or Decrypt data with key and nonce
-   *
-   * @param {Uint8Array} data
-   * @return {Uint8Array}
-   * @private
-   */
-  _update(data) {
+  if (!(nonce instanceof Uint8Array) || nonce.length !== 8) {
+    throw new Error('Nonce should be 8 byte array!')
+  }
 
-    if (!(data instanceof Uint8Array) || data.length === 0) {
-      throw new Error("Data should be type of bytes (Uint8Array) and not empty!");
+  this.rounds = 20
+  this.sigma = [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574]
+  this.param = [
+    // Constant
+    this.sigma[0],
+    // Key
+    this._get32(key, 0),
+    this._get32(key, 4),
+    this._get32(key, 8),
+    this._get32(key, 12),
+    this.sigma[1],
+    // Nonce
+    this._get32(nonce, 0),
+    this._get32(nonce, 4),
+    // Counter
+    0,
+    0,
+    // Constant
+    this.sigma[2],
+    // Key
+    this._get32(key, 16),
+    this._get32(key, 20),
+    this._get32(key, 24),
+    this._get32(key, 28),
+    // Const
+    this.sigma[3]
+  ]
+
+  // init block 64 bytes //
+  this.block = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  ]
+
+  // internal byte counter //
+  this.byteCounter = 0
+}
+
+/**
+ *  Encrypt or Decrypt data with key and nonce
+ *
+ * @param {Uint8Array} data
+ * @return {Uint8Array}
+ * @private
+ */
+JSSalsa20.prototype._update = function (data) {
+  if (!(data instanceof Uint8Array) || data.length === 0) {
+    throw new Error('Data should be type of bytes (Uint8Array) and not empty!')
+  }
+
+  var output = new Uint8Array(data.length)
+
+  // core function, build block and xor with input data //
+  for (var i = 0; i < data.length; i++) {
+    if (this.byteCounter === 0 || this.byteCounter === 64) {
+      this._salsa()
+      this._counterIncrement()
+      this.byteCounter = 0
     }
 
-    const output = new Uint8Array(data.length);
-
-    // core function, build block and xor with input data //
-    for (let i = 0; i < data.length; i++) {
-      if (this.byteCounter === 0 || this.byteCounter === 64) {
-        this._salsa();
-        this._counterIncrement();
-        this.byteCounter = 0;
-      }
-
-      output[i] = data[i] ^ this.block[this.byteCounter++];
-    }
-
-    return output;
+    output[i] = data[i] ^ this.block[this.byteCounter++]
   }
 
-  /**
-   *  Encrypt data with key and nonce
-   *
-   * @param {Uint8Array} data
-   * @return {Uint8Array}
-   */
-  encrypt(data) {
-    return this._update(data);
-  }
+  return output
+}
+/**
+ *  Encrypt data with key and nonce
+ *
+ * @param {Uint8Array} data
+ * @return {Uint8Array}
+ */
+JSSalsa20.prototype.encrypt = function (data) {
+  return this._update(data)
+}
 
-  /**
-   *  Decrypt data with key and nonce
-   *
-   * @param {Uint8Array} data
-   * @return {Uint8Array}
-   */
-  decrypt(data) {
-    return this._update(data);
-  }
+/**
+ *  Decrypt data with key and nonce
+ *
+ * @param {Uint8Array} data
+ * @return {Uint8Array}
+ */
+JSSalsa20.prototype.decrypt = function (data) {
+  return this._update(data)
+}
 
-  _counterIncrement() {
-    // Max possible blocks is 2^64
-    this.param[8] = (this.param[8] + 1) >>> 0;
-    if (this.param[8] == 0) {
-      this.param[9] = (this.param[9] + 1) >>> 0;
-    }
-  }
-
-  _salsa() {
-    const mix = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let i = 0, b = 0;
-
-    // copy param array to mix //
-    for (i = 0; i < 16; i++) {
-      mix[i] = this.param[i];
-    }
-
-    // mix rounds //
-    for (i = 0; i < this.rounds; i += 2) {
-      this._quarter(mix, 4, 8, 12, 0);
-      this._quarter(mix, 9, 13, 1, 5);
-      this._quarter(mix, 14, 2, 6, 10);
-      this._quarter(mix, 3, 7, 11, 15);
-      this._quarter(mix, 1, 2, 3, 0);
-      this._quarter(mix, 6, 7, 4, 5);
-      this._quarter(mix, 11, 8, 9, 10);
-      this._quarter(mix, 12, 13, 14, 15);
-    }
-
-    for (i = 0; i < 16; i++) {
-      // add
-      mix[i] += this.param[i];
-
-      // store
-      this.block[b++] = mix[i] & 0xFF;
-      this.block[b++] = (mix[i] >>> 8) & 0xFF;
-      this.block[b++] = (mix[i] >>> 16) & 0xFF;
-      this.block[b++] = (mix[i] >>> 24) & 0xFF;
-    }
-  }
-
-  /**
-   * Salsa quarter function
-   *
-   * @param {[number]} data
-   * @param {number} a
-   * @param {number} b
-   * @param {number} c
-   * @param {number} d
-   * @private
-   */
-  _quarter(data, a, b, c, d) {
-    // >>> 0 convert double to unsigned integer 32 bit
-    data[a] = (data[a] ^ this._rotl(data[d] + data[c], 7)) >>> 0;
-    data[b] = (data[b] ^ this._rotl(data[a] + data[d], 9)) >>> 0;
-    data[c] = (data[c] ^ this._rotl(data[b] + data[a], 13)) >>> 0;
-    data[d] = (data[d] ^ this._rotl(data[c] + data[b], 18)) >>> 0;
-  }
-
-  /**
-   * Little-endian to uint 32 bytes
-   *
-   * @param {Uint8Array|[number]} data
-   * @param {number} index
-   * @return {number}
-   * @private
-   */
-  _get32(data, index) {
-    return data[index++] ^ (data[index++] << 8) ^ (data[index++] << 16) ^ (data[index] << 24);
-  }
-
-  /**
-   * Cyclic left rotation
-   *
-   * @param {number} data
-   * @param {number} shift
-   * @return {number}
-   * @private
-   */
-  _rotl(data, shift) {
-    return ((data << shift) | (data >>> (32 - shift)));
+JSSalsa20.prototype._counterIncrement = function () {
+  // Max possible blocks is 2^64
+  this.param[8] = (this.param[8] + 1) >>> 0
+  if (this.param[8] === 0) {
+    this.param[9] = (this.param[9] + 1) >>> 0
   }
 }
 
+JSSalsa20.prototype._salsa = function () {
+  var mix = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  var i = 0
+  var b = 0
+
+  // copy param array to mix //
+  for (i = 0; i < 16; i++) {
+    mix[i] = this.param[i]
+  }
+
+  // mix rounds //
+  for (i = 0; i < this.rounds; i += 2) {
+    mix[4] = (mix[4] ^ this._rotl(mix[0] + mix[12], 7)) >>> 0
+    mix[8] = (mix[8] ^ this._rotl(mix[4] + mix[0], 9)) >>> 0
+    mix[12] = (mix[12] ^ this._rotl(mix[8] + mix[4], 13)) >>> 0
+    mix[0] = (mix[0] ^ this._rotl(mix[12] + mix[8], 18)) >>> 0
+    mix[9] = (mix[9] ^ this._rotl(mix[5] + mix[1], 7)) >>> 0
+    mix[13] = (mix[13] ^ this._rotl(mix[9] + mix[5], 9)) >>> 0
+    mix[1] = (mix[1] ^ this._rotl(mix[13] + mix[9], 13)) >>> 0
+    mix[5] = (mix[5] ^ this._rotl(mix[1] + mix[13], 18)) >>> 0
+    mix[14] = (mix[14] ^ this._rotl(mix[10] + mix[6], 7)) >>> 0
+    mix[2] = (mix[2] ^ this._rotl(mix[14] + mix[10], 9)) >>> 0
+    mix[6] = (mix[6] ^ this._rotl(mix[2] + mix[14], 13)) >>> 0
+    mix[10] = (mix[10] ^ this._rotl(mix[6] + mix[2], 18)) >>> 0
+    mix[3] = (mix[3] ^ this._rotl(mix[15] + mix[11], 7)) >>> 0
+    mix[7] = (mix[7] ^ this._rotl(mix[3] + mix[15], 9)) >>> 0
+    mix[11] = (mix[11] ^ this._rotl(mix[7] + mix[3], 13)) >>> 0
+    mix[15] = (mix[15] ^ this._rotl(mix[11] + mix[7], 18)) >>> 0
+    mix[1] = (mix[1] ^ this._rotl(mix[0] + mix[3], 7)) >>> 0
+    mix[2] = (mix[2] ^ this._rotl(mix[1] + mix[0], 9)) >>> 0
+    mix[3] = (mix[3] ^ this._rotl(mix[2] + mix[1], 13)) >>> 0
+    mix[0] = (mix[0] ^ this._rotl(mix[3] + mix[2], 18)) >>> 0
+    mix[6] = (mix[6] ^ this._rotl(mix[5] + mix[4], 7)) >>> 0
+    mix[7] = (mix[7] ^ this._rotl(mix[6] + mix[5], 9)) >>> 0
+    mix[4] = (mix[4] ^ this._rotl(mix[7] + mix[6], 13)) >>> 0
+    mix[5] = (mix[5] ^ this._rotl(mix[4] + mix[7], 18)) >>> 0
+    mix[11] = (mix[11] ^ this._rotl(mix[10] + mix[9], 7)) >>> 0
+    mix[8] = (mix[8] ^ this._rotl(mix[11] + mix[10], 9)) >>> 0
+    mix[9] = (mix[9] ^ this._rotl(mix[8] + mix[11], 13)) >>> 0
+    mix[10] = (mix[10] ^ this._rotl(mix[9] + mix[8], 18)) >>> 0
+    mix[12] = (mix[12] ^ this._rotl(mix[15] + mix[14], 7)) >>> 0
+    mix[13] = (mix[13] ^ this._rotl(mix[12] + mix[15], 9)) >>> 0
+    mix[14] = (mix[14] ^ this._rotl(mix[13] + mix[12], 13)) >>> 0
+    mix[15] = (mix[15] ^ this._rotl(mix[14] + mix[13], 18)) >>> 0
+  }
+
+  for (i = 0; i < 16; i++) {
+    // add
+    mix[i] += this.param[i]
+
+    // store
+    this.block[b++] = mix[i] & 0xFF
+    this.block[b++] = (mix[i] >>> 8) & 0xFF
+    this.block[b++] = (mix[i] >>> 16) & 0xFF
+    this.block[b++] = (mix[i] >>> 24) & 0xFF
+  }
+}
+
+/**
+ * Little-endian to uint 32 bytes
+ *
+ * @param {Uint8Array|[number]} data
+ * @param {number} index
+ * @return {number}
+ * @private
+ */
+JSSalsa20.prototype._get32 = function (data, index) {
+  return data[index++] ^ (data[index++] << 8) ^ (data[index++] << 16) ^ (data[index] << 24)
+}
+
+/**
+ * Cyclic left rotation
+ *
+ * @param {number} data
+ * @param {number} shift
+ * @return {number}
+ * @private
+ */
+JSSalsa20.prototype._rotl = function (data, shift) {
+  return ((data << shift) | (data >>> (32 - shift)))
+}
+
 // EXPORT //
-if (typeof module !== "undefined" && module.exports) {
-  module.exports = JSSalsa20;
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = JSSalsa20
 }
